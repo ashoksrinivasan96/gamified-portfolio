af5a98950732c0d2db9f259d7f8ef1a08f7bbec6
Ashok srinivasan gurumurthy <agurumurthy@agurumu-ltmf82e.internal.salesforce.com>
2023-11-02 10:49:09 +0530

    added player movememt

--- gamifiedportfolio/src/components/Player.js
+++ gamifiedportfolio/src/components/Player.js
@@ -2,6 +2,14 @@ import { useState, useEffect } from "react"
 
 const Player = ({canvasContext}) => {
 const gravity = 1.5;
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 const [player, setPlayer] = useState({
         position:{
             x: 100,
@@ -24,6 +32,7 @@ const [player, setPlayer] = useState({
     const update = () => {
         draw();
         player.position.y+=player.velocity.y;
+        player.position.x+=player.velocity.x;
         if(player.position.y + player.height + player.velocity.y <=canvasContext.canvas.height){
             player.velocity.y+=gravity;
         }
@@ -36,9 +45,59 @@ const [player, setPlayer] = useState({
         requestAnimationFrame(animate);
         canvasContext.context.clearRect(0,0,canvasContext.canvas.width,canvasContext.canvas.height);
         update();
+
+        if(keys.right.pressed){
+            player.velocity.x = 5;
+        }
+        else if(keys.left.pressed){
+            player.velocity.x = -5;
+        }
+        else player.velocity.x = 0;
+
+
     }
 
  animate();
+
+ window.addEventListener('keydown', ({keyCode}) => {
+    switch(keyCode) {
+        case 65:
+            console.log("THis is left");
+            keys.left.pressed = true
+            break;
+        case 83:
+            console.log("THis is down");
+            break;
+        case 68:
+            console.log("THis is right");
+            keys.right.pressed = true
+            break;
+        case 87:
+            console.log("THis is up");
+            player.velocity.y -= 20;
+            break;                 
+    }
+ })
+
+ window.addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode) {
+        case 65:
+            console.log("THis is left");
+            keys.left.pressed = false
+            break;
+        case 83:
+            console.log("THis is down");
+            break;
+        case 68:
+            console.log("THis is right");
+            keys.right.pressed = false
+            break;
+        case 87:
+            console.log("THis is up");
+            player.velocity.y -= 20;
+            break;                 
+    }
+ })
 
 }
 
