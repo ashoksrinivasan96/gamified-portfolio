dab66fecb37b0031514428d1a4db785d0ac3b5a5
ashoksrinivasan96 <ashoksrinivasan96@gmail.com>
2023-11-14 21:11:04 +0530

    Added the main menu

--- gamifiedportfolio/src/components/Card.js
+++ gamifiedportfolio/src/components/Card.js
@@ -4,20 +4,18 @@ import scroll from '../assets/scroll.png';
 
 const Card = () => {
   return (
-    <div className="p-48 m-48 bg-contain bg-no-repeat relative overflow-hidden">
-      <div
-        className="absolute inset-0 flex items-center justify-center bg-cover"
-        style={{ backgroundImage: `url(${scroll})` }}
-      >
-        <ul className="text-center font-proxima text-white">
+    
+      <div className='bg-center bg-cover min-h-[40rem] min-w-[60rem]' style={{ backgroundImage: `url(${scroll})` }}>
+        <ul className= "text-center font-mono text-white pt-[10rem]">
           <li>Use the 'W', 'A', 'S', 'D' keys to control the player.</li>
           <li>Press 'W' to jump, 'A' to move left, 'D' to move right.</li>
-          <li>Avoid falling off the platforms, collect codex to gain information about the candidate.</li>
+          <li>Avoid falling off the platforms</li>
+          <li>collect codex to gain information about the candidate.</li>
           <li>Defeat various enemies.</li>
           <li>Fight the final boss.</li>
         </ul>
       </div>
-    </div>
+
   );
 };
 
--- gamifiedportfolio/src/components/MainMenu.js
+++ gamifiedportfolio/src/components/MainMenu.js
@@ -5,21 +5,21 @@ import mainMenu from '../assets/mainmenu.jpg';
 
 const MainMenu = ({ startGame }) => {
   return (
-    <div className="h-screen bg-cover" style={{ backgroundImage: `url(${mainMenu})` }}>
-    <div className="flex flex-col items-center justify-left h-screen bg-gradient-to-b text-white">
-        <div className="p-36 rounded-lg shadow-lg text-center">
+    <div className="bg-center" style={{ backgroundImage: `url(${mainMenu})` }}>
+      <div className="flex flex-col items-center justify-center text-white">
+        <div className="mt-[13rem] rounded-lg shadow-lg text-center">
           <h1 className="text-5xl font-extrabold text-yellow-300 mb-6">Talent Hunter</h1>
           <div className="text-lg mb-6">
             <p className="text-yellow-200">Welcome, Recruiters! Are you ready to hunt top talent? Follow these instructions:</p>
           </div>
-          <Card />
-          <button
-            onClick={startGame}
-            className="bg-yellow-300 hover:bg-yellow-500 text-green-900 font-bold py-2 px-6 rounded-full focus:outline-none"
-          >
-            Hunt
-          </button>
         </div>
+        <Card />
+        <button
+          onClick={startGame}
+          className="bg-yellow-300 hover:bg-yellow-500 text-green-900 font-bold py-2 px-6 rounded-full focus:outline-none mt-8"
+        >
+          Hunt
+        </button>
       </div>
     </div>
   );
